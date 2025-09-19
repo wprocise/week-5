a4a08f49a30d3270e84b47e68b805156a6b2637c
3durfey <3durfey@gmail.com>
2025-09-19 12:10:14 -0400

    minor changes

--- app.py
+++ app.py
@@ -2,24 +2,26 @@ import streamlit as st
 
 from apputil import *
 
-
-st.write(
-'''
-# Week x: [Title]
-
-...
-''')
-
 st.write(
 '''
 # Week 5: Titanic Survival Visualization
-
 Explore survival rates by class and gender.
 '''
 )
 
 # Load Titanic dataset
+df = pd.read_csv('https://raw.githubusercontent.com/leontoddjohnson/datasets/main/data/titanic.csv')
 
 # Generate and display the figure
 fig = plot_survival_by_age_class_gender()
+st.plotly_chart(fig, use_container_width=True)
+
+st.write(
+'''
+# Week 5: Titanic Visualization 2
+'''
+)
+
+# Generate and display the figure
+fig2 = exercise2()
 st.plotly_chart(fig, use_container_width=True)
