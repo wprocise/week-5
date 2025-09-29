940cec9c89b51af46ee8735d48c4ebfd73432db8
Woods Procise <wprocise@iu.edu>
2025-09-29 17:02:14 -0500

    making imports

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 from apputil import *
 
 # Load Titanic dataset
--- apputil.py
+++ apputil.py
@@ -1,5 +1,13 @@
+# Importing necessary libraries
 import plotly.express as px
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
+## EXERCISE 1: SURVIVAL PATTERNS
 
-# update/add code below ...
+# Load the Titanic dataset
+df = pd.read_csv('https://raw.githubusercontent.com/leontoddjohnson/datasets/main/data/titanic.csv')
+
+# 1) Create a new column in the Titanic dataset that classifies passengers into age categories: 'Child' (0-12), 'Teen' (13-19), 'Adult' (20-59), and 'Senior' (60+)
