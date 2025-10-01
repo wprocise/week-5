21c26bbbbabc81b11368cf2f54542c8a441b4c5c
Woods Procise <wprocise@iu.edu>
2025-09-30 21:03:06 -0400

    Created Titanic visualization that answers my question. Used a bar chart

--- app.py
+++ app.py
@@ -12,7 +12,17 @@ st.write("How did survival rates vary among different age groups on the Titanic?
 
 '''
 )
+# 7) Create a Plotly visualization that addresses the question
 # Generate and display the figure
+def visualize_demographic():
+	fig1 = px.bar(summary, x='Age_group', y='survival_rate',
+			 title='Survival Rate by Age Group and Sex on the Titanic',
+			 labels={'Age_group': 'Age Group', 'survival_rate': 'Survival Rate'},
+			 color='Sex',
+			 color_discrete_map={'male': 'blue', 'female': 'pink'},
+			 barmode='group')
+	return fig1
+
 fig1 = visualize_demographic()
 st.plotly_chart(fig1, use_container_width=True)
 
