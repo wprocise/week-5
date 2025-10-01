0e58ce9acbfeb72cb8df39c26b040dab3d011398
Woods Procise <wprocise@iu.edu>
2025-09-30 20:38:10 -0400

    Grouped and calculated

--- apputil.py
+++ apputil.py
@@ -15,7 +15,7 @@ df = pd.read_csv('https://raw.githubusercontent.com/leontoddjohnson/datasets/mai
     demographic categories: class, sex, age group
 
     Returns: Table with results of all combinations of demographic categories
-    """
+"""
 
 # 1) Create a new column in the Titanic dataset that classifies passengers into age categories: 'Child' (0-12), 'Teen' (13-19), 'Adult' (20-59), and 'Senior' (60+)
 """
@@ -24,12 +24,26 @@ df = pd.read_csv('https://raw.githubusercontent.com/leontoddjohnson/datasets/mai
 
     age bins = [0, 12, 19, 59, np.inf]
     labels = ['Child', 'Teen', 'Adult', 'Senior']
-    """
+"""
 df['Age_group'] = pd.cut(df['Age'], bins=[0, 12, 19, 59, np.inf], labels=['Child', 'Teen', 'Adult', 'Senior'])
 
 # 2) Group the passengers by class, sex, and age group
-# group
+"""
+    Use groupby to group the passengers
+"""
 grouped = df.groupby(['Pclass', 'Sex', 'Age_group'])
+
+# 3) For each group, calculate the total number of passengers, survivors, and survival rate
+"""Use agg to aggregate the grouped data
+    Calculating total passengers(n_passengers), survivors(n_survivors), and survival rate(survival_rate)
+"""
+summary = grouped.agg(
+    n_passengers=('Survived', 'count'),
+    n_survivors=('Survived', 'sum')
+).reset_index()
+summary['survival_rate'] = summary['n_survivors'] / summary['n_passengers']
+
+
 
 
 
